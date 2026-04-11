31f51f168765f3328d496544551c78cb32cf7738
Bamidele Adeyemo <bam81190@gmail.com>
2026-04-11 11:44:56 -0400

    Initial Commit

---
+++ MatchKeeper/MatchKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchKeeperApp.swift
+//  MatchKeeper
+//
+//  Created by Bamidele Adeyemo on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MatchKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
